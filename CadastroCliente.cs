d2a0e44aa215068df7e9c64a59db0ba50a95a617
pedro <Pedrotaroco1@gmail.com>
2025-09-05 10:24:18 -0300

    Atividade de Cadastro de cliente, validações

--- CadastroCliente.cs
+++ CadastroCliente.cs
@@ -28,42 +28,89 @@ namespace Cadastro_de_Clientes
             string nome = textBoxNome.Text;
             string sobrenome = textBoxSobrenome.Text;
             string email = textBoxEmail.Text;
-            int idade = int.Parse(textBoxIdade.Text);
-            string conexaoBanco = "server=localhost;user id=root;password=;database=db_clientes";
+            string idadeString = textBoxIdade.Text;
+            string arroba = "@";
 
-            using (MySqlConnection conexao = new MySqlConnection(conexaoBanco))
+            bool validarIdade = int.TryParse(idadeString, out int idade);
+
+            
+
+            if(String.IsNullOrEmpty(nome)  )
             {
-                try
+                    MessageBox.Show("Dados Inválidos,O campo Nome está incorreto, Digite Novamente o Nome", "Dados Cliente", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                textBoxNome.Focus();
+                return;
+            }else if (String.IsNullOrEmpty(sobrenome))
+            {
+                MessageBox.Show("Dados Inválidos,O campo Sobrenome está incorreto, Digite Novamente o Sobrenome", "Dados Cliente", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                textBoxSobrenome.Focus();
+                return;
+            }
+            else if (String.IsNullOrEmpty(email))
+            {
+                MessageBox.Show("Dados Inválidos,O campo Email está incorreto, Digite Novamente o Email", "Dados Cliente", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                textBoxEmail.Focus();
+                return;
+            }
+            else if(email.Contains(arroba) == false)
+            {
+                MessageBox.Show("Dados Inválidos,O campo Email Precisa de @,Digite Novamente o Email", "Dados Cliente", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                textBoxEmail.Focus();
+                return;
+            }
+            else if (validarIdade == false || idade >= 130 || idade <= 0)
+            {
+                MessageBox.Show("Dados Inválidos,O campo Idade está incorreto, Digite Novamente a Idade", "Dados Cliente", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                textBoxIdade.Focus();
+                return;
+            }
+            else
+            {
+                string conexaoBanco = "server=localhost;user id=root;password=;database=db_clientes";
+
+                using (MySqlConnection conexao = new MySqlConnection(conexaoBanco))
                 {
-                    conexao.Open();
-                    string query = "INSERT INTO tb_clientes (nome, sobrenome, email, idade) VALUES (@nome, @sobrenome, @email, @idade)";
+                    try
+                    {
+                        conexao.Open();
+                        string query = "INSERT INTO tb_clientes (nome, sobrenome, email, idade) VALUES (@nome, @sobrenome, @email, @idade)";
 
-                    MySqlCommand cmd = new MySqlCommand(query, conexao);
-                    cmd.Parameters.AddWithValue("@nome", nome);
-                    cmd.Parameters.AddWithValue("@sobrenome", sobrenome);
-                    cmd.Parameters.AddWithValue("@email", email);
-                    cmd.Parameters.AddWithValue("@idade", idade);
-                    cmd.ExecuteNonQuery();
-                    MessageBox.Show($"Cliente {nome} Cadastrado Com Sucesso", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        MySqlCommand cmd = new MySqlCommand(query, conexao);
+                        cmd.Parameters.AddWithValue("@nome", nome);
+                        cmd.Parameters.AddWithValue("@sobrenome", sobrenome);
+                        cmd.Parameters.AddWithValue("@email", email);
+                        cmd.Parameters.AddWithValue("@idade", idade);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show($"Cliente {nome} Cadastrado Com Sucesso", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                    textBoxNome.Clear();
-                    textBoxEmail.Clear();
-                    textBoxSobrenome.Clear();
-                    textBoxIdade.Clear();
-                    textBoxNome.Focus();
+                        limparDados();
 
-                    conexao.Close();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Erro ao Cadastrar Cliente, Erro de conexão com o banco de dados" + ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        conexao.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro ao Cadastrar Cliente, Erro de conexão com o banco de dados" + ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                 }
             }
+
+                idade = int.Parse(idadeString);
+
+            
         }
 
         private void Fechar_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void limparDados()
+        {
+            textBoxNome.Clear();
+            textBoxEmail.Clear();
+            textBoxSobrenome.Clear();
+            textBoxIdade.Clear();
+            textBoxNome.Focus();
         }
     }
 }
---
+++ banco_sql/db_clientes.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04/09/2025 às 15:49
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db_clientes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_clientes`
+--
+
+CREATE TABLE `tb_clientes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(200) DEFAULT NULL,
+  `sobrenome` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `idade` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_clientes`
+--
+
+INSERT INTO `tb_clientes` (`id`, `nome`, `sobrenome`, `email`, `idade`) VALUES
+(1, 'Pedro', 'Augusto', 'Pedro.sdabshad@bshdaasb', 18),
+(2, 'Victor', 'Chinagli', 'ChinagliaDev.dhabdks', 12);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_clientes`
+--
+ALTER TABLE `tb_clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_clientes`
+--
+ALTER TABLE `tb_clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
