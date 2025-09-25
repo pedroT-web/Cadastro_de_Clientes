5c72bbd8727ce1b44e48cee407c837476dd0dccd
pedro <Pedrotaroco1@gmail.com>
2025-09-25 11:57:15 -0300

    Atualizando funcionalidades do projeto

--- ConsultaCliente.Designer.cs
+++ ConsultaCliente.Designer.cs
@@ -29,35 +29,29 @@
         private void InitializeComponent()
         {
             this.GridClientes = new System.Windows.Forms.DataGridView();
-            this.Listar = new System.Windows.Forms.Button();
             this.Fechar = new System.Windows.Forms.Button();
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.buttonAtualizar = new System.Windows.Forms.Button();
+            this.comboBoxNome = new System.Windows.Forms.ComboBox();
+            this.GridNomeSolo = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.GridClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridNomeSolo)).BeginInit();
             this.SuspendLayout();
             // 
             // GridClientes
             // 
             this.GridClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.GridClientes.Location = new System.Drawing.Point(34, 23);
+            this.GridClientes.Location = new System.Drawing.Point(34, 322);
             this.GridClientes.Name = "GridClientes";
-            this.GridClientes.Size = new System.Drawing.Size(646, 564);
+            this.GridClientes.Size = new System.Drawing.Size(535, 199);
             this.GridClientes.TabIndex = 0;
-            // 
-            // Listar
-            // 
-            this.Listar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Listar.Location = new System.Drawing.Point(756, 185);
-            this.Listar.Name = "Listar";
-            this.Listar.Size = new System.Drawing.Size(175, 62);
-            this.Listar.TabIndex = 1;
-            this.Listar.Text = "Listar Clientes";
-            this.Listar.UseVisualStyleBackColor = true;
-            this.Listar.Click += new System.EventHandler(this.Listar_Click);
+            this.GridClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridClientes_CellContentClick);
             // 
             // Fechar
             // 
             this.Fechar.BackColor = System.Drawing.Color.Red;
             this.Fechar.ForeColor = System.Drawing.Color.White;
-            this.Fechar.Location = new System.Drawing.Point(756, 298);
+            this.Fechar.Location = new System.Drawing.Point(742, 538);
             this.Fechar.Name = "Fechar";
             this.Fechar.Size = new System.Drawing.Size(175, 49);
             this.Fechar.TabIndex = 2;
@@ -65,18 +59,62 @@
             this.Fechar.UseVisualStyleBackColor = false;
             this.Fechar.Click += new System.EventHandler(this.Fechar_Click);
             // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonApagar.Location = new System.Drawing.Point(742, 193);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(175, 62);
+            this.buttonApagar.TabIndex = 3;
+            this.buttonApagar.Text = "Apagar Clientes";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            this.buttonApagar.Click += new System.EventHandler(this.buttonApagar_Click);
+            // 
+            // buttonAtualizar
+            // 
+            this.buttonAtualizar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAtualizar.Location = new System.Drawing.Point(742, 278);
+            this.buttonAtualizar.Name = "buttonAtualizar";
+            this.buttonAtualizar.Size = new System.Drawing.Size(175, 62);
+            this.buttonAtualizar.TabIndex = 4;
+            this.buttonAtualizar.Text = "Atualizar Dados";
+            this.buttonAtualizar.UseVisualStyleBackColor = true;
+            this.buttonAtualizar.Click += new System.EventHandler(this.buttonAtualizar_Click);
+            // 
+            // comboBoxNome
+            // 
+            this.comboBoxNome.FormattingEnabled = true;
+            this.comboBoxNome.Location = new System.Drawing.Point(34, 38);
+            this.comboBoxNome.Name = "comboBoxNome";
+            this.comboBoxNome.Size = new System.Drawing.Size(535, 21);
+            this.comboBoxNome.TabIndex = 5;
+            this.comboBoxNome.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // GridNomeSolo
+            // 
+            this.GridNomeSolo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridNomeSolo.Location = new System.Drawing.Point(34, 87);
+            this.GridNomeSolo.Name = "GridNomeSolo";
+            this.GridNomeSolo.Size = new System.Drawing.Size(535, 69);
+            this.GridNomeSolo.TabIndex = 6;
+            this.GridNomeSolo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewClienteSolo_CellContentClick);
+            // 
             // ConsultaCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1031, 616);
+            this.Controls.Add(this.GridNomeSolo);
+            this.Controls.Add(this.comboBoxNome);
+            this.Controls.Add(this.buttonAtualizar);
+            this.Controls.Add(this.buttonApagar);
             this.Controls.Add(this.Fechar);
-            this.Controls.Add(this.Listar);
             this.Controls.Add(this.GridClientes);
             this.Name = "ConsultaCliente";
             this.Text = "ConsultaCliente";
             this.Load += new System.EventHandler(this.ConsultaCliente_Load);
             ((System.ComponentModel.ISupportInitialize)(this.GridClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridNomeSolo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -84,7 +122,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView GridClientes;
-        private System.Windows.Forms.Button Listar;
         private System.Windows.Forms.Button Fechar;
+        private System.Windows.Forms.Button buttonApagar;
+        private System.Windows.Forms.Button buttonAtualizar;
+        private System.Windows.Forms.ComboBox comboBoxNome;
+        private System.Windows.Forms.DataGridView GridNomeSolo;
     }
 }
--- ConsultaCliente.cs
+++ ConsultaCliente.cs
@@ -22,7 +22,8 @@ namespace Cadastro_de_Clientes
 
         private void ConsultaCliente_Load(object sender, EventArgs e)
         {
-
+            fnListarClientes();
+            fnCarregarComboBox();
         }
 
         private void Fechar_Click(object sender, EventArgs e)
@@ -32,8 +33,55 @@ namespace Cadastro_de_Clientes
 
         private void Listar_Click(object sender, EventArgs e)
         {
-            string conexaoBanco = "server=localhost;user id=root;password=;database=db_clientes";
+            fnListarClientes();
+        }
 
+        private void GridClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonApagar_Click(object sender, EventArgs e)
+        {
+            if (GridClientes.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecione um cliente para executar a remoção do mesmo", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            DataGridViewRow linhaSelecionada = GridClientes.SelectedRows[0];
+            int idCliente = Convert.ToInt32(linhaSelecionada.Cells["id"].Value);
+
+            DialogResult confirmacao = MessageBox.Show("Relmente Deseja Deleter o cliente?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirmacao == DialogResult.Yes)
+            {
+                string conexaoBanco = "server=localhost;user id=root;database=db_clientes";
+                using (MySqlConnection conexao = new MySqlConnection(conexaoBanco))
+                {
+                    try
+                    {
+                        conexao.Open();
+                        string delete = "DELETE FROM tb_clientes WHERE id = @id"; // Linha de execussão de exclusão
+                        MySqlCommand cmd = new MySqlCommand(delete, conexao); // criando um novo comando 
+                        cmd.Parameters.AddWithValue("@id", idCliente); // Adicionando o valor do id para a variável temporaria
+                        cmd.ExecuteNonQuery(); // execução do comando
+
+                        conexao.Close(); // fechando o banco
+                        fnListarClientes(); // instanciando a função para ser executada depois de deletar o cliente;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro ao deletar cliente, erro de conexão de banco de dados", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+            }
+        }
+
+        private void fnListarClientes()
+        {
+            string conexaoBanco = "server=localhost;user id=root;password=;database=db_clientes";
             using (MySqlConnection conexao = new MySqlConnection(conexaoBanco))
             {
                 try
@@ -55,6 +103,98 @@ namespace Cadastro_de_Clientes
                     MessageBox.Show("Erro ao Cadastrar Cliente, Erro de conexão com o banco de dados" + ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+        }
+
+        private void buttonAtualizar_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow linhaSelecionada = GridClientes.SelectedRows[0];
+            int idCliente = Convert.ToInt32(linhaSelecionada.Cells["id"].Value);
+            string nomeCliente = Convert.ToString(linhaSelecionada.Cells["nome"].Value);
+            string sobreNomeCliente = Convert.ToString(linhaSelecionada.Cells["sobrenome"].Value);
+            string emailCliente = Convert.ToString(linhaSelecionada.Cells["email"].Value);
+            int idadeCliente = Convert.ToInt32(linhaSelecionada.Cells["idade"].Value);
+
+            DialogResult confirmacao = MessageBox.Show("Relmente Deseja Atualizar o cliente?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirmacao == DialogResult.Yes)
+            {
+                string conexaoBanco = "server=localhost;user id=root;database=db_clientes";
+                using (MySqlConnection conexao = new MySqlConnection(conexaoBanco))
+                {
+                    try
+                    {
+                        conexao.Open();
+                        string delete = "UPDATE tb_clientes SET nome = @nome, sobrenome = @sobrenome, email = @email, idade = @idade WHERE id = @id"; // Linha de execussão de exclusão
+                        MySqlCommand cmd = new MySqlCommand(delete, conexao); // criando um novo comando 
+                        cmd.Parameters.AddWithValue("@id", idCliente); // Adicionando o valor do id para a variável temporaria
+                        cmd.Parameters.AddWithValue("@nome", nomeCliente);
+                        cmd.Parameters.AddWithValue("@sobrenome", sobreNomeCliente);
+                        cmd.Parameters.AddWithValue("@email", emailCliente);
+                        cmd.Parameters.AddWithValue("@idade", idadeCliente);
+                        cmd.ExecuteNonQuery(); // execução do comando
+
+                        conexao.Close(); // fechando o banco
+                        fnListarClientes(); // instanciando a função para ser executada depois de deletar o cliente;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro ao deletar cliente, erro de conexão de banco de dados", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fnCarregarComboBox()
+        {
+            string conexaoBanco = "server=localhost;user id=root;database=db_clientes";
+            using (MySqlConnection conn = new MySqlConnection(conexaoBanco))
+            {
+                try
+                {
+                    conn.Open();
+                    string script = "SELECT id, nome FROM tb_clientes";
+                    MySqlCommand cmd = new MySqlCommand(script, conn);
+                    MySqlDataAdapter adaptador = new MySqlDataAdapter(cmd);
+                    DataTable tabela = new DataTable();
+                    adaptador.Fill(tabela);
+                    comboBoxNome.ValueMember = "id";
+                    comboBoxNome.DisplayMember = "nome";
+                    comboBoxNome.DataSource = tabela;
+
+
+                    int id_nome = Convert.ToInt32(comboBoxNome.ValueMember);
+                    string scriptSolo = "SELECT * FROM tb_clientes WHERE id = @id";
+                    MySqlCommand cmdSolo = new MySqlCommand(scriptSolo, conn);
+                    cmd.Parameters.AddWithValue("@id", id_nome);
+                    MySqlDataAdapter adaptadorSolo = new MySqlDataAdapter(cmd);
+                    DataTable tabelaSolo = new DataTable();
+                    adaptador.Fill(tabelaSolo);
+
+                    GridNomeSolo.DataSource = tabelaSolo;
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro de conexão com o banco de dados", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void dataGridViewClienteSolo_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private string conexaoBanco()
+        {
+            string conexaoBanco = "server=localhost;user id=root;database=db_clientes";
+
+            return conexaoBanco;
         }
     }
 }
